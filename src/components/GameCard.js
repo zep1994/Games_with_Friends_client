8f78b83be02e41410714d573dd2fedbaec6133cf
zep1994 <snaxbags@protonmail.com>
2018-12-27 17:19:16 -0600

    Add Gameshow page

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const GameCard = ({game}) => (
-    <div key={game.id} className="GameCard">
-        <img className="Gameimg" src={game.image_url} alt={game.name} />
-        <h3>{game.name}</h3> 
-        <p>{game.drinks}</p>
-        <p>Directions: {game.directions}</p>
-    </div>
+    <Link to={`/games/${game.id}`} exact >
+        <div key={game.id} className="GameCard">
+            <img className="Gameimg" src={game.image_url} alt={game.name} />
+            <h3>{game.name}</h3> 
+            <p>{game.drinks}</p>
+            <p>Directions: {game.directions}</p>
+        </div>
+    </Link>
 )
 
 export default GameCard
---
+++ src/components/GamePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GamePage = ({game}) => (
+    <h1>{game.name} {game.drinks}</h1>
+)
+
+export default GamePage
