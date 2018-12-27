21046fd8cceff683e66acb3afca11d131a26c3b9
zep1994 <snaxbags@protonmail.com>
2018-12-27 17:14:31 -0600

    Add Game image url

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const GameCard = ({game}) => (
     <div key={game.id} className="GameCard">
+        <img className="Gameimg" src={game.image_url} alt={game.name} />
         <h3>{game.name}</h3> 
         <p>{game.drinks}</p>
         <p>Directions: {game.directions}</p>
