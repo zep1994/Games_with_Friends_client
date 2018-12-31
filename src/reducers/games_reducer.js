3542da7aaeb1209dcf94e43ebe43a8926b1f904b
zep1994 <snaxbags@protonmail.com>
2018-12-31 11:54:28 -0600

    Fix Delete Game

--- src/reducers/games_reducer.js
+++ src/reducers/games_reducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch(action.type){
         case 'GET_GAMES':
             return action.games
+        case 'REMOVE_GAME':
+            return state.filter(game => game.id !== action.game.id)
         default:
             return state
     }
